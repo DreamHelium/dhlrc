be0e9ae512d97d9c6412cb5f0ccfb135a3298c93
孟小骇 <48383878+DreamHelium@users.noreply.github.com>
2025-02-12 00:41:59 +0800

    [0.3.4-pre] Allow dynamically modify Recipe

--- src/qt/recipesui.cpp
+++ src/qt/recipesui.cpp
@@ -15,11 +15,12 @@
 #include <qnamespace.h>
 #include "../translation.h"
 #include "../recipe_handler/handler.h"
+#include "dh_string_util.h"
+#include "glib.h"
 #include "glibconfig.h"
 #include "recipesshowui.h"
 #include <QFileDialog>
 #include "../config.h"
-#include "../uncompress.h"
 #include <QAbstractItemView>
 #include <QDebug>
 #include "../common_info.h"
@@ -46,53 +47,24 @@ RecipesUI::RecipesUI(gchar* uuid, QWidget *parent) :
     ItemList* il = (ItemList*)common_info_get_data(DH_TYPE_Item_List, uuid);
     il_uuid = uuid;
     ilr = il;
-    recipesInit();
+    gchar* recipeDir = dh_get_recipe_dir();
+    recipesInit(recipeDir);
+    g_free(recipeDir);
     initUI();
 }
 
 RecipesUI::~RecipesUI()
 {
     list.clear();
-    delete[] rpl;
+    rpl.clear();
     common_info_writer_unlock(DH_TYPE_Item_List, il_uuid);
 }
 
-void RecipesUI::recipesInit()
+void RecipesUI::recipesInit(QString path)
 {
-    gchar* recipeDir = dh_get_recipe_dir();
-    qDebug() << recipeDir;
-    RecipeList* rcl = recipe_list_init(recipeDir, ilr);
-    if(!rcl)
-    {
-        auto btn = QMessageBox::question(this, _("No recipe file found!"), 
-        _("No recipe file found! Do you want to extract game file?"));
-        if(btn == QMessageBox::Yes)
-        {
-            /* Extract file from game dir */
-            char* gameDir = dh_get_game_dir();
-            char* version = dh_get_version();
-            /* gamedir / "versions" / version / version ".jar" */
-            /* %1      %2 versions  %2 %3     %2 %3      .jar*/
-            QString originGameJar = QString("%1%2versions%2%3%2%3.jar").arg(gameDir).arg(G_DIR_SEPARATOR).arg(version);
-            QString realGameJar = QFileDialog::getOpenFileName(this, _("Please select a game jar"), originGameJar, _("Jar File (*.jar)"));
-
-            char* cacheDir = dh_get_cache_dir();
-            /* cachedir / version / "extracted" / */
-            /* %1       %2 %3     %2 extracted  %2 */
-            QString destDir = QString("%1%2%3%2extracted%2").arg(cacheDir).arg(G_DIR_SEPARATOR).arg(version);
-            dh_file_create(destDir.toStdString().c_str(), FALSE);
-            dhlrc_extract(realGameJar.toUtf8(), destDir.toStdString().c_str());
-            QString sourceDir = destDir;
-            sourceDir += "/data/minecraft/recipes/";
-            g_free(gameDir);
-            g_free(version);
-            g_free(cacheDir);
-            dh_file_copy_dir(sourceDir.toStdString().c_str(), recipeDir, G_FILE_COPY_NONE);
-            rcl = recipe_list_init(recipeDir, ilr);
-        }
-    }
-    g_free(recipeDir);
+    RecipeList* rcl = recipe_list_init(path.toUtf8(), ilr);
     RecipeList* rcl_d = rcl;
+    list.clear();
     while(rcl_d)
     {
         bool haveData = false;
@@ -111,18 +83,15 @@ void RecipesUI::recipesInit()
         }
         if(!haveData)
         {
-            RecipesInternal* ri = new RecipesInternal;
-            ri->itemName = rclName;
-            ri->filenames << data->filename;
-            ri->num = item_list_get_item_num(ilr, (char*)rclName.toStdString().c_str());
-            list.append(*ri);
-            delete ri; /* Can I do this? */
+            RecipesInternal ri;
+            ri.itemName = rclName;
+            ri.filenames << data->filename;
+            ri.num = item_list_get_item_num(ilr, (char*)rclName.toStdString().c_str());
+            list.append(ri);
         }
         rcl_d = rcl_d ->next;
     }
     recipe_list_free(rcl);
-
-    ItemList* ild = ilr;
 }
 
 void RecipesUI::initUI()
@@ -133,6 +102,7 @@ void RecipesUI::initUI()
     QWidget* widget = new QWidget(this);
     al->addWidget(area);
 
+    getRecipe = new QPushButton(_("No items? Get recipe files from file."));
     label1 = new QLabel(_("Please select items:"));
     lineedit = new QLineEdit();
     lineedit->setPlaceholderText(_("Search block name"));
@@ -140,79 +110,18 @@ void RecipesUI::initUI()
     widget->setLayout(allLayout);
 
     QObject::connect(lineedit, &QLineEdit::textChanged, this, &RecipesUI::search_text_changed);
+    QObject::connect(getRecipe, &QPushButton::clicked, this, &RecipesUI::getRecipe_clicked);
 
+    allLayout->addWidget(getRecipe);
     allLayout->addWidget(label1);
     allLayout->addWidget(lineedit);
     allLayout->addStretch();
 
     area->setWidgetResizable(true);
 
-    rpl = new rp[list.length()];
-
-    for(int i = 0 ; i < list.length() ; i++)
-    {
-        QString str = trm(list[i].itemName.toStdString().c_str());
-        str += " ";
-        str += QString::number(list[i].num);
-
-        rpl[i].checkBox = new QCheckBox();
-        rpl[i].slider = new QSlider(Qt::Horizontal);
-        rpl[i].textEdit = new QLineEdit();
-        rpl[i].comboBox = new QComboBox();
-        rpl[i].recipesLayout = new QHBoxLayout();
-        rpl[i].recipesWidget = new QWidget();
-        rpl[i].recipesBtn = new QPushButton(_("Show recipe"));
-
-        rpl[i].checkBox->setText(str);
-        allLayout->addWidget(rpl[i].checkBox);
-
-        rpl[i].slider->setMinimum(0);
-        rpl[i].slider->setMaximum(list[i].num);
-        rpl[i].slider->setValue(list[i].num);
-        rpl[i].slider->setFixedWidth(100);
-
-        rpl[i].textEdit->setText(QString::number(list[i].num));
-        QValidator* validator = new QIntValidator(0, list[i].num, this);
-        rpl[i].textEdit->setValidator(validator);
-        rpl[i].textEdit->setFixedWidth(50);
-
-        QStringList filenamesStr;
-
-        for(int j = 0 ; j < list[i].filenames.length() ; j++)
-        {
-            QString filename = list[i].filenames[j];
-            QStringList splitNames = filename.split(G_DIR_SEPARATOR);
-            filenamesStr.append(splitNames[splitNames.length()-1]);
-        }
-        
-        rpl[i].comboBox->addItems(filenamesStr);
-        auto view = rpl[i].comboBox->view();
-        int maxWidth = 0;
-        for(int j = 0 ; j < rpl[i].comboBox->count() ; j++)
-        {
-            int width = view->sizeHintForColumn(j);
-            maxWidth = MAX(maxWidth, width);
-        }
-        view->setFixedWidth(maxWidth);
-
-        QSize size = fontMetrics().size(Qt::TextSingleLine, rpl[i].recipesBtn->text());
-
-        rpl[i].recipesBtn->setFixedWidth(size.width() + 10);
-
-        rpl[i].recipesLayout->addWidget(rpl[i].slider);
-        rpl[i].recipesLayout->addWidget(rpl[i].textEdit);
-        rpl[i].recipesLayout->addWidget(rpl[i].comboBox);
-        rpl[i].recipesLayout->addWidget(rpl[i].recipesBtn);
-        rpl[i].recipesWidget->setLayout(rpl[i].recipesLayout);
-
-
-        allLayout->addWidget(rpl[i].recipesWidget);
-        rpl[i].recipesWidget->hide();
-        QObject::connect(rpl[i].checkBox, SIGNAL(clicked()), this, SLOT(checkbox_clicked()));
-        QObject::connect(rpl[i].slider, SIGNAL(valueChanged(int)), this, SLOT(slider_changed(int)));
-        QObject::connect(rpl[i].textEdit, &QLineEdit::textChanged, this, &RecipesUI::text_changed);
-        QObject::connect(rpl[i].recipesBtn, &QPushButton::pressed, this, &RecipesUI::recipesbtn_clicked);
-    }
+    recipesLayout = new QVBoxLayout();
+    drawLayout();
+    allLayout->addLayout(recipesLayout);
     area->setWidget(widget);
 
     askLayout = new QVBoxLayout();
@@ -300,7 +209,7 @@ void RecipesUI::okbtn_clicked()
     }
     if(askForCombineBox->isChecked() == false)
     {
-        common_info_new(DH_TYPE_Region, new_il, g_date_time_new_now_local(), des2Edit->text().toStdString().c_str());
+        common_info_new(DH_TYPE_Item_List, new_il, g_date_time_new_now_local(), des2Edit->text().toStdString().c_str());
     }
     else
     {
@@ -398,4 +307,158 @@ void RecipesUI::search_text_changed(const QString &a)
             else rpl[i].recipesWidget->hide();
         }
     }
+}
+
+void RecipesUI::getRecipe_clicked()
+{
+    gchar* recipeDir = dh_get_recipe_dir();
+    /* Extract file from game dir */
+    char* gameDir = dh_get_game_dir();
+    char* version = dh_get_version();
+    /* gamedir / "versions" / version / version ".jar" */
+    /* %1      %2 versions  %2 %3     %2 %3      .jar*/
+    QString originGameJar = QString("%1%2versions%2%3%2%3.jar").arg(gameDir).arg(G_DIR_SEPARATOR).arg(version);
+    QString realGameJar = QFileDialog::getOpenFileName(this, _("Please select a game jar"), originGameJar, _("Jar File (*.jar)"));
+
+    char* cacheDir = dh_get_cache_dir();
+    /* cachedir / version / "extracted" / */
+    /* %1       %2 %3     %2 extracted  %2 */
+    QString destDir = QString("%1%2%3%2extracted%2").arg(cacheDir).arg(G_DIR_SEPARATOR).arg(version);
+    dh_file_create(destDir.toUtf8(), FALSE);
+    extract_local(realGameJar, destDir);
+    g_free(gameDir);
+    g_free(version);
+    g_free(cacheDir);
+    g_free(recipeDir);
+    // dhlrc_extract(realGameJar.toUtf8(), destDir.toUtf8());
+}
+
+void RecipesUI::drawLayout()
+{
+    for(auto i : rpl)
+    {
+        recipesLayout->removeWidget(i.recipesWidget);
+        recipesLayout->removeWidget(i.checkBox);
+        delete i.recipesWidget;
+        delete i.checkBox;
+    }
+    rpl.clear();
+    for(int i = 0 ; i < list.length() ; i++)
+    {
+        QString str = trm(list[i].itemName.toStdString().c_str());
+        str += " ";
+        str += QString::number(list[i].num);
+
+        rp r;
+
+        r.checkBox = new QCheckBox();
+        r.slider = new QSlider(Qt::Horizontal);
+        r.textEdit = new QLineEdit();
+        r.comboBox = new QComboBox();
+        r.recipesLayout = new QHBoxLayout();
+        r.recipesWidget = new QWidget();
+        r.recipesBtn = new QPushButton(_("Show recipe"));
+        rpl.append(r);
+
+        rpl[i].checkBox->setText(str);
+        recipesLayout->addWidget(rpl[i].checkBox);
+
+        rpl[i].slider->setMinimum(0);
+        rpl[i].slider->setMaximum(list[i].num);
+        rpl[i].slider->setValue(list[i].num);
+        rpl[i].slider->setFixedWidth(100);
+
+        rpl[i].textEdit->setText(QString::number(list[i].num));
+        QValidator* validator = new QIntValidator(0, list[i].num, this);
+        rpl[i].textEdit->setValidator(validator);
+        rpl[i].textEdit->setFixedWidth(50);
+
+        QStringList filenamesStr;
+
+        for(int j = 0 ; j < list[i].filenames.length() ; j++)
+        {
+            QString filename = list[i].filenames[j];
+            QStringList splitNames = filename.split(G_DIR_SEPARATOR);
+            filenamesStr.append(splitNames[splitNames.length()-1]);
+        }
+        
+        rpl[i].comboBox->addItems(filenamesStr);
+        auto view = rpl[i].comboBox->view();
+        int maxWidth = 0;
+        for(int j = 0 ; j < rpl[i].comboBox->count() ; j++)
+        {
+            int width = view->sizeHintForColumn(j);
+            maxWidth = MAX(maxWidth, width);
+        }
+        view->setFixedWidth(maxWidth);
+
+        QSize size = fontMetrics().size(Qt::TextSingleLine, rpl[i].recipesBtn->text());
+
+        rpl[i].recipesBtn->setFixedWidth(size.width() + 10);
+
+        rpl[i].recipesLayout->addWidget(rpl[i].slider);
+        rpl[i].recipesLayout->addWidget(rpl[i].textEdit);
+        rpl[i].recipesLayout->addWidget(rpl[i].comboBox);
+        rpl[i].recipesLayout->addWidget(rpl[i].recipesBtn);
+        rpl[i].recipesWidget->setLayout(rpl[i].recipesLayout);
+
+
+        recipesLayout->addWidget(rpl[i].recipesWidget);
+        rpl[i].recipesWidget->hide();
+        QObject::connect(rpl[i].checkBox, SIGNAL(clicked()), this, SLOT(checkbox_clicked()));
+        QObject::connect(rpl[i].slider, SIGNAL(valueChanged(int)), this, SLOT(slider_changed(int)));
+        QObject::connect(rpl[i].textEdit, &QLineEdit::textChanged, this, &RecipesUI::text_changed);
+        QObject::connect(rpl[i].recipesBtn, &QPushButton::pressed, this, &RecipesUI::recipesbtn_clicked);
+    }
+}
+
+static void finish_extract(GPid pid, gint status, gpointer user_data)
+{
+    RecipesUI* rui = (RecipesUI*)user_data;
+    g_spawn_close_pid(pid);
+    if(status != 0)
+    {
+        g_critical(_("Unzip failed!"));
+        return;
+    }
+    else
+    {
+        QString sourceDir = rui->tempDir;
+        sourceDir += "/data/minecraft/recipes/";
+        gchar* recipeDir = dh_get_recipe_dir();
+        
+        QString recipeDirQS = QFileDialog::getExistingDirectory(rui, _("Please select a destination."), recipeDir);
+        g_free(recipeDir);
+        dh_file_copy_dir(sourceDir.toUtf8(), recipeDirQS.toUtf8(), G_FILE_COPY_NONE);
+        rui->recipesInit(recipeDirQS);
+        rui->drawLayout();
+    }
+}
+
+void RecipesUI::extract_local(QString srcpath, QString destpath)
+{
+    if(!dh_file_exist(srcpath.toUtf8()))
+        dh_file_create(srcpath.toUtf8(), FALSE);
+    const char* destdir = destpath.toUtf8();
+    char* unzipDir = g_find_program_in_path("unzip");
+    if(unzipDir)
+    {
+        GPid pid;
+        GError* err = NULL;
+        DhStrArray* arr = NULL;
+        dh_str_array_add_str(&arr, unzipDir);
+        dh_str_array_add_str(&arr, "-q");
+        dh_str_array_add_str(&arr, srcpath.toUtf8());
+        g_free(unzipDir);
+        if(!g_spawn_async(destdir, arr->val, NULL, G_SPAWN_DO_NOT_REAP_CHILD, NULL, NULL, &pid, &err))
+        {
+            dh_str_array_free(arr);
+            g_critical("%s", err->message);
+            g_error_free(err);
+        }
+        dh_str_array_free(arr);
+        tempDir = destpath;
+        g_child_watch_add(pid, finish_extract, this);
+    }
+    else QMessageBox::critical(this, _("No Unzip Found!"), _("No unzip program found!"));
 }
--- src/qt/recipesui.h
+++ src/qt/recipesui.h
@@ -37,22 +37,29 @@ class RecipesUI : public QWidget
 public:
     explicit RecipesUI(gchar* uuid, QWidget *parent = nullptr);
     ~RecipesUI();
+    QString tempDir;
+    void recipesRefresh();
+    void drawLayout();
+    void recipesInit(QString path);
 
 private:
     QLabel* label1;
     QLineEdit* lineedit;
 
-    rp* rpl = nullptr;
+    QList<rp> rpl;
     ItemList* ilr;
     const char* il_uuid;
     QVBoxLayout* al;
     QVBoxLayout* allLayout;
     QHBoxLayout* buttonLayout;
+    QVBoxLayout* recipesLayout;
     QVBoxLayout* askLayout;
     QCheckBox* askForCombineBox;
     QLabel* des2;
     QLineEdit* des2Edit;
     QHBoxLayout* des2Layout;
+
+    QPushButton* getRecipe;
 
     QPushButton* okBtn;
     QPushButton* closeBtn;
@@ -60,8 +67,9 @@ private:
     QScrollArea* area;
 
     void initUI();
-    void recipesInit();
+    
     ItemList* recipesProcess(const char* item, const char* filepos ,quint32 num);
+    void extract_local(QString srcpath, QString destpath);
 
 private Q_SLOTS:
     void checkbox_clicked();
@@ -71,6 +79,7 @@ private Q_SLOTS:
     void recipesbtn_clicked();
     void afcb_clicked(bool checked);
     void search_text_changed(const QString &a);
+    void getRecipe_clicked();
 
 protected:
     void resizeEvent(QResizeEvent* event);
